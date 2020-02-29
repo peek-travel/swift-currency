cf39c185cf80384da3c7cb28a45d774aa1ce0c21
Nathan Harris <nathan.harris@peek.com>
2020-02-28 21:02:36 -0800

    Refactor CurrencyMint (#21)
    Motivation:
    
    The current implementation of CurrencyMint is highly rigid and requires a ton of support from SwiftCurrency to be of true value.
    
    Cases of custom currencies and quick fallbacks to a default type requires a fair bit of boilerplate code that can be simplified with
    an improved re-implementation.
    
    In addition, there were several places of code duplication that are possible to simplify with Swift's literal syntax for easier maintenance.
    
    Modifications:
    
    - Add: `CurrencyIdentifier` for CurrencyMint to use in lookup resolution methods
    - Add: `standard` static singleton for CurrencyMint to resolve only ISO 4217 currencies
    - Change: `CurrencyMint` to store a fallback lookup closure for 3rd party and default currency resolution
    
    Result:
    
    Developers using CurrencyMint should have an easier time supporting their own currencies, or unsupported/missing ISO 4217 currencies.

--- Tests/CurrencyTests/CurrencyMintTests.swift
+++ Tests/CurrencyTests/CurrencyMintTests.swift
@@ -17,46 +17,80 @@ import XCTest
 
 final class CurrencyMintTests: XCTestCase {
   func testLookupByString_passes() {
-    let mint = CurrencyMint()
-    let pounds = mint.make(alphabeticCode: "GBP")
+    let pounds = CurrencyMint.standard.make(identifier: "GBP")
+    XCTAssertTrue(pounds is GBP)
     XCTAssertEqual(pounds?.amount, .zero)
   }
   
   func testLookupByString_withAmount() {
-    let mint = CurrencyMint()
+    let mint = CurrencyMint.standard
     
-    let yen = mint.make(alphabeticCode: "JPY", amount: 302.98)
+    let yen = mint.make(identifier: "JPY", amount: 302.98)
+    XCTAssertTrue(yen is JPY)
     XCTAssertEqual(yen?.amount, 303)
     
-    let usd = mint.make(alphabeticCode: "USD", minorUnits: 549)
+    let usd = mint.make(identifier: "USD", minorUnits: 549)
+    XCTAssertTrue(usd is USD)
     XCTAssertEqual(usd?.amount, 5.49)
   }
   
   func testLookupByString_fails() {
-    let mint = CurrencyMint()
-    let talons = mint.make(alphabeticCode: "KLT")
-    XCTAssertNil(talons)
+    let darseks = CurrencyMint.standard.make(identifier: "KED", amount: 30.23)
+    XCTAssertNil(darseks)
   }
   
   func testLookupByNum_passes() {
-    let mint = CurrencyMint()
-    let euros = mint.make(numericCode: 978)
+    let euros = CurrencyMint.standard.make(identifier: 978)
+    XCTAssertTrue(euros is EUR)
     XCTAssertEqual(euros?.amount, .zero)
   }
   
   func testLookupByNum_withAmount() {
-    let mint = CurrencyMint()
+    let mint = CurrencyMint.standard
     
-    let pesos = mint.make(numericCode: 484, amount: 3098.9823)
+    let pesos = mint.make(identifier: 484, amount: 3098.9823)
+    XCTAssertTrue(pesos is MXN)
     XCTAssertEqual(pesos?.amount, 3098.98)
     
-    let omanis = mint.make(numericCode: 512, minorUnits: 198239)
+    let omanis = mint.make(identifier: 512, minorUnits: 198239)
+    XCTAssertTrue(omanis is OMR)
     XCTAssertEqual(omanis?.amount, 198.239)
   }
   
   func testLookupByNum_fails() {
-    let mint = CurrencyMint()
-    let talons = mint.make(numericCode: 666)
-    XCTAssertNil(talons)
+    let darseks = CurrencyMint.standard.make(identifier: 666)
+    XCTAssertNil(darseks)
+  }
+  
+  func testDefaultCurrency() {
+    let mint = CurrencyMint(defaultCurrency: USD.self)
+    let money = mint.make(identifier: "KED")
+    XCTAssertTrue(money is USD)
+  }
+  
+  func testFallbackLookup() {
+    struct KED: CurrencyProtocol, CurrencyMetadata {
+      static var name: String { return "Klingon Darseks" }
+      static var alphabeticCode: String { return "KED" }
+      static var numericCode: UInt16 { return 666 }
+      static var minorUnits: UInt8 { return 3 }
+      
+      var minorUnits: Int64
+
+      public init<T : BinaryInteger>(minorUnits: T) { self.minorUnits = .init(minorUnits) }
+    }
+    
+    let mint = CurrencyMint(fallbackLookup: { identifier in
+      guard identifier == .alphaCode("KED") || identifier == .numericCode(666) else { return nil }
+      return KED.self
+    })
+    
+    let d1 = mint.make(identifier: "KED", amount: 30.239)
+    XCTAssertTrue(d1 is KED)
+    XCTAssertEqual(d1 as? KED, 30.239)
+    
+    let d2 = mint.make(identifier: 666, minorUnits: d1?.minorUnits ?? .zero)
+    XCTAssertTrue(d2 is KED)
+    XCTAssertEqual(d2?.minorUnits, 30239)
   }
 }
