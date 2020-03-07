be54905cb4c53317db043e7938c8918a4842a97d
Nathan Harris <nathan.harris@peek.com>
2020-03-06 20:50:08 -0800

    Improve currency localization ergonomics (#34)
    Motivation:
    
    String interpolation is powerful, but the syntax is not particularly discoverable as there is no code completion assistance, at least in Xcode.
    
    In addition, the parameter labels don't match the Swift API Design Guidelines.
    
    Modifications:
    
    - Add: `localizedString(for:nilDescription:)` to `AnyCurrency` that uses the String interpolation implementation
    - Add: `localizedString(using:nilDescription:)` to `AnyCurrency` that uses the String interpolation implementation
    - Change: `(localize:withFormatter:nilDescription:)` to `(localize:with:nilDescription:)`
    - Change: `(localize:forLocale:nilDescription:)` to `(localize:for:nilDescription:)`
    
    Result:
    
    Developers should have an easier time discovering the localization capabilities of currencies much easier, with more options for generating localized strings.

--- Sources/Currency/AnyCurrency.swift
+++ Sources/Currency/AnyCurrency.swift
@@ -186,23 +186,80 @@ extension AnyCurrency {
 
 // MARK: String Representation
 
-// price.description                  3000.98 USD
-// price.debugDescription         USD(3000.98)
-// price.playgroundDescription    USD(3000.98)
-// "\(localize: price)"                       $3,000.98
-// "\(localize: price, withFormatter: ...)"   $3,000.98
-// "\(localize: price, forLocale: .current)"  $3,000.98
+// price.description                    3000.98 USD
+// price.debugDescription           USD(3000.98)
+// price.playgroundDescription      USD(3000.98)
+// "\(localize: price)"               $3,000.98
+// "\(localize: price, with: ...)"    $3,000.98
+// "\(localize: price, for: ...)"     $3,000.98
 
 extension AnyCurrency {
   public var description: String { return "\(self.amount.description) \(Self.metadata.alphabeticCode)" }
   public var debugDescription: String { return "\(Self.metadata.alphabeticCode)(\(self.amount.description))"}
   public var playgroundDescription: Any { return self.debugDescription }
+
+  /// Creates a string representation of the currency value, localized to a particular locale.
+  ///
+  ///       let usd = USD(30.03)
+  ///       print(usd.localizedString(for: .init(identifier: "fr_FR")))
+  ///       // 30,03 $US
+  ///       print(usd.localizedString())
+  ///       // $30.03, assuming `Locale.current` is "en_US"
+  ///
+  /// - Note: This can also be done with String interpolation:
+  ///
+  ///       let pounds = GBP(30.03)
+  ///       let localizedString = "\(localize: pounds, for: Locale(identifier: "de_DE"))"
+  ///       print(localizedString)
+  ///       // 30,03 £
+  ///       print("\(localize: pounds)")
+  ///       // £30.03, assuming `Locale.current` is "en_US"
+  ///
+  /// - Parameters:
+  ///   - locale: The Locale to localize the value for. The default is `.current`, ig. the runtime environment's Locale.
+  ///   - nilValue: The String representation to use if the value is `nil` or localization fails. The default is `"nil"`.
+  /// - Returns: A localized String representation of the currency value.
+  public func localizedString(for locale: Locale = .current, nilDescription nilValue: String = "nil") -> String {
+    return "\(localize: self, for: locale, nilDescription: nilValue)"
+  }
+
+  /// Creates a string representation of the currency value, using the provided formatter.
+  ///
+  ///       let formatter = NumberFormatter()
+  ///       formatter.numberStyle = .currency
+  ///       formatter.currencyGroupingSeparator = " "
+  ///       formatter.currencyDecimalSeparator = "'"
+  ///       formatter.currencyCode = "GBP"
+  ///
+  ///       let pounds = GBP(14928.02)
+  ///       print(pounds.localizedString(using: formatter))
+  ///       // £14 928'02
+  ///
+  ///- Note: This can also be done with String interpolation:
+  ///
+  ///       let formatter = ...
+  ///       let currency = ...
+  ///       let localizedString = "\(localize: currency, with: formatter)"
+  ///
+  /// - Important: Since `Foundation.NumberFormatter` is a class, this method does not set the `currencyCode` property automatically.
+  ///
+  /// If it did, this method would no longer be thread safe, and would mutate the provided instance.
+  ///
+  /// If the same formatter is to be used for different currencies, the property will need to be updated before calling this method.
+  ///
+  /// - Parameters:
+  ///   - formatter: The pre-configured formatter to use.
+  ///   - nilValue: The String representation to use if the value is `nil` or localization fails. The default is `"nil"`.
+  /// - Returns: A localized String representation of the currency value.
+  public func localizedString(using formatter: NumberFormatter, nilDescription nilValue: String = "nil") -> String {
+    return "\(localize: self, with: formatter, nilDescription: nilValue)"
+  }
 }
 
 extension String.StringInterpolation {
   public mutating func appendInterpolation<Currency: AnyCurrency>(
     localize value: Currency?,
-    forLocale locale: Locale = .current,
+    for locale: Locale = .current,
     nilDescription nilValue: String = "nil"
   ) {
     guard case let .some(value) = value else { return nilValue.write(to: &self) }
@@ -212,12 +269,12 @@ extension String.StringInterpolation {
     formatter.locale = locale
     formatter.currencyCode = Currency.metadata.alphabeticCode
 
-    self.appendInterpolation(localize: value, withFormatter: formatter, nilDescription: nilValue)
+    self.appendInterpolation(localize: value, with: formatter, nilDescription: nilValue)
   }
 
   public mutating func appendInterpolation<Currency: AnyCurrency>(
     localize value: Currency?,
-    withFormatter formatter: NumberFormatter,
+    with formatter: NumberFormatter,
     nilDescription nilValue: String = "nil"
   ) {
     guard case let .some(value) = value else { return nilValue.write(to: &self) }
