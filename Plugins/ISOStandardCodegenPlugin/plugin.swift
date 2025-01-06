d65dcd44f8dbeac2c05110de55110fe0b3818755
Nathan Harris <nathan@mordil.info>
2025-01-06 11:09:46 -0600

    Replace CSV with JSON as file format for ISO Currency definitions (#43)
    Motivation:
    
    We're pulling in a dependency for CSV parsing as part of the build tool,
    forcing a transitive dependency on all users.
    
    We simply need a way of storing the original definitions from the ISO
    Standard committee and parsing them as part of the build process, and
    JSON + `Codable` solves that for us easily.
    
    Modifications:
    
    - Change: Code to be a bit more legible on what's going on with the
    codegen
    - Change: Data parsing from CSV to JSON using `Decodable`
    - Rename: Files to be a bit more easily understandable of what's in each
    file
    - Remove: **SwiftCSV** dependency
    
    Result:
    
    The build plugin is a bit easier to maintain, and there is now no
    dependencies in the project.

--- Plugins/ISOStandardCodegenPlugin/plugin.swift
+++ Plugins/ISOStandardCodegenPlugin/plugin.swift
@@ -2,7 +2,7 @@
 //
 // This source file is part of the SwiftCurrency open source project
 //
-// Copyright (c) 2022 SwiftCurrency project authors
+// Copyright (c) 2024 SwiftCurrency project authors
 // Licensed under MIT License
 //
 // See LICENSE.txt for license information
@@ -17,21 +17,21 @@ import PackagePlugin
 @main
 struct ISOCurrencies: BuildToolPlugin {
     func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
-      let csvInputPath = context.package.directory.appending("ISO4217.csv")
+      let dataInputPath = context.package.directory.appending("ISO4217.json")
 
       let currencyDefinitionPath = context.pluginWorkDirectory.appending("ISOCurrencies.swift")
-      let mintDefinitionPath = context.pluginWorkDirectory.appending("CurrencyMint+ISOCurrencies.swift")
+      let mintDefinitionPath = context.pluginWorkDirectory.appending("CurrencyMint+ISOCurrencyLookup.swift")
 
       return [.buildCommand(
         displayName: "Generating ISO Standard currency support code",
         executable: try context.tool(named: "ISOStandardCodegen").path,
         arguments: [
-          csvInputPath.string,
+          dataInputPath.string,
           currencyDefinitionPath.string,
           mintDefinitionPath.string
         ],
         inputFiles: [
-          csvInputPath
+          dataInputPath
         ],
         outputFiles: [
           currencyDefinitionPath,
