a9a9e39b40959ab46d79c8216b73b0cbe9577fab
Nathan Harris <nathan.harris@peek.com>
2025-01-07 13:17:39 -0600

    Simplify CurrencyValue protocol to absolute bare requirements

--- Sources/Currency/CurrencyValue.swift
+++ Sources/Currency/CurrencyValue.swift
@@ -35,13 +35,6 @@ public protocol CurrencyValue:
   /// The information describing the currency.
   static var descriptor: CurrencyDescriptor.Type { get }
 
-  /// The amount represented as a whole number of the curreny's "minor units".
-  ///
-  /// For example, as the USD uses 1/100 for its minor unit,
-  /// with the value of `USD 1.01`, ``minorUnits`` will be the value `101`.
-  /// - Important: Fractional values (of the currency) are impossible to be represented in this value.
-  var minorUnits: CurrencyMinorUnitRepresentation { get }
-
   /// The exact amount of money being represented,
   /// even if the value is fractional of what the currency uses for its minor units.
   ///
@@ -50,61 +43,64 @@ public protocol CurrencyValue:
   /// For an amount that is likely to be used in "every day" usage, see ``roundedAmount``
   var exactAmount: Decimal { get }
 
-  /// The cannonical representation of the ``exactAmount`` of money this value represents,
-  /// rounded using the `bankers` method to the currency's ``CurrencyDescriptor/minorUnits``.
-  ///
-  /// For example:
-  ///
-  ///     let usd = USD(10.007)
-  ///     let yen = JPY(100.9)
-  ///     let dinar = KWD(100.0019)
-  ///     print(usd, yen, dinar)
-  ///     // "USD(10.01), JPY(101), KWD(100.002)
-  var roundedAmount: Decimal { get }
-
   /// Creates a representation of the currency with the exact value as given.
   /// - Parameter exactAmount: The amount this instance should represent, without any rounding.
   init(exactAmount: Decimal)
+}
+
+// MARK: Defaults
+
+extension CurrencyValue where Self: CurrencyDescriptor {
+  public static var descriptor: CurrencyDescriptor.Type { Self.self }
+}
+
+// MARK: Extensions
+
+extension CurrencyValue {
+  /// The amount represented as a whole number of the curreny's "minor units".
+  ///
+  /// For example, as the USD uses 1/100 for its minor unit,
+  /// with the value of `USD 1.01`, ``minorUnits`` will be the value `101`.
+  /// - Important: Fractional values of the currency are truncated.
+  public var minorUnits: CurrencyMinorUnitRepresentation {
+    let scaledAmount = self.exactAmount * Self.descriptor.minorUnitsCoefficient(for: .exactAmount)
+    return .init(scaledAmount.int64Value)
+  }
+
+  /// The "every day" representation of the ``exactAmount`` of money this value represents,
+  /// rounded using the `bankers` method to the currency's ``CurrencyDescriptor/minorUnits``.
+  ///
+  /// For example:
+  /// ```swift
+  ///     let usd = USD(10.007)
+  ///     let yen = JPY(100.9)
+  ///     let dinar = KWD(100.0019)
+  ///
+  ///     print(usd, yen, dinar)
+  ///     // "USD(10.01), JPY(101), KWD(100.002)
+  /// ```
+  public var roundedAmount: Decimal { self.roundedAmount(using: .bankers) }
 
   /// Creates a representation of the currency, converting the minor units representation to a decimal format.
   ///
   /// e.g. If you provide the value `100` for a currency with `2` minor units, the `exactAmount` will be `1.00`.
   /// - Parameter minorUnits: The minor units this value will represent.
-  init(minorUnits: CurrencyMinorUnitRepresentation)
+  public init(minorUnits: CurrencyMinorUnitRepresentation) {
+    let amount = Decimal(minorUnits) * Self.descriptor.minorUnitsCoefficient(for: .minorUnits)
+    self.init(exactAmount: amount)
+  }
 
   /// Rounds the ``exactAmount`` of money being represented using the given rounding method
   /// to the currency's ``CurrencyDescriptor/minorUnits``.
   /// - Parameter roundingMode: The desired rounding mode to use on the original ``exactAmount``.
   /// - Complexity: O(1)
   /// - Returns: The rounded amount.
-  func roundedAmount(using roundingMode: NSDecimalNumber.RoundingMode) -> Decimal
-}
-
-// MARK: Defaults
-
-extension CurrencyValue {
-  public var minorUnits: CurrencyMinorUnitRepresentation {
-    let scaledAmount = self.exactAmount * Self.descriptor.minorUnitsCoefficient(for: .exactAmount)
-    return .init(scaledAmount.int64Value)
-  }
-
-  public var roundedAmount: Decimal { self.roundedAmount(using: .bankers) }
-
-  public init(minorUnits: CurrencyMinorUnitRepresentation) {
-    let amount = Decimal(minorUnits) * Self.descriptor.minorUnitsCoefficient(for: .minorUnits)
-    self.init(exactAmount: amount)
-  }
-
   public func roundedAmount(using roundingMode: NSDecimalNumber.RoundingMode) -> Decimal {
     var sourceAmount = self.exactAmount
     var result = Decimal.zero
     NSDecimalRound(&result, &sourceAmount, .init(Self.descriptor.minorUnits), roundingMode)
     return result
   }
-}
-
-extension CurrencyValue where Self: CurrencyDescriptor {
-  public static var descriptor: CurrencyDescriptor.Type { Self.self }
 }
 
 // MARK: Equatable
